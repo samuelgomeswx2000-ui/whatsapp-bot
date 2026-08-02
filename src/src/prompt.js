6331862dd08022235a9d98e24cd42788206d3a00
samuelgomeswx2000-ui <samuelgomeswx2000@gmail.com>
2026-08-02 08:55:26 -0300

    Add welcome and farewell messages to prompt.js

---
+++ src/src/prompt.js
@@ -0,0 +1,29 @@
+export const DONO_NOME = process.env.DONO_NOME || "Samuel Gomes";
+export const DONO_CONTATO = process.env.DONO_CONTATO || "+55 (12) 98841-2285";
+
+export const MENSAGEM_BOAS_VINDAS = `Olá! 👋
+
+Seja bem-vindo ao atendimento automático.
+
+Digite /menu para visualizar todas as opções.
+
+Caso precise de atendimento humano, fale com o responsável:
+${DONO_CONTATO}.`;
+
+export const MENSAGEM_DESPEDIDA = `Obrigado pelo contato!
+
+Sempre que precisar estaremos à disposição.
+
+Até logo! 👋`;
+
+export const REGRAS_TEXTO = `📋 *Regras de atendimento*
+
+- Não divulgamos informações falsas.
+- Não enviamos spam.
+- Não ofendemos usuários.
+- Respeitamos a privacidade.
+- Sempre respondemos com educação.`;
+
+export const MENSAGEM_PADRAO = `Não entendi sua mensagem. 🤔
+
+Digite /menu para ver as opções disponíveis.`;
